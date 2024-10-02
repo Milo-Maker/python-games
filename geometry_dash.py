ff0520471d6c7677fa97118ab20c3029fc7b4192
Milo <milo.choron@gmail.com>
2024-10-02 21:44:16 +0200

    ajout graphisme images block et logo

--- geometry_dash.py
+++ geometry_dash.py
@@ -29,20 +29,20 @@ deffilx=0
 O=1
 I=2
 map=[
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,O,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,O,0,0,0,0,0,0,O,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-[0,0,0,0,0,0,0,0,0,0,0,0,0,0,I,0,0,0,0,0,0,0,0,0,I,O,I,I,I,I,I,I,I,0,0,0,0,0,0,0]
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,O,O,O,O,O,0,0,0,O,O,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,O,0,0,0,0,0,0,O,0,0,0,0,0,0,0,O,0,0,0,0,0,O,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,],
+[0,0,0,0,0,0,0,0,0,0,0,0,0,0,I,0,0,0,0,0,0,0,0,0,I,O,I,I,I,I,I,I,I,O,I,I,I,I,I,O,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,]
 ]
 position_spike=[]
 position_block=[]
-for i in range(len(map)):
+for i in range(len(map)):  
     for j in range(len(map[0])):
         if map[i][j]==1:
             position_block.append([j*70,i*70])
@@ -60,25 +60,32 @@ text_police=pygame.font.SysFont("Arial",30)
 def affiche_text(text,police,text_col,x,y):
     img = police.render(text,True,text_col)
     fenetre.blit(img,(x,y))
+logo=pygame.image.load("img/Logo_géométrie_dash.png")
+block=pygame.image.load("img/Block_Géométrie_dash.png")
 
 def menu(col_ecriture):
+    global logo
     fenetre.fill(blanc)
+    fenetre.blit(logo,(500,70))
     affiche_text("appuie sur espace pour continuer",text_police,col_ecriture,600,700)
+    
+    
 
-def draw_spikes(fenetre):
+def draw_spikes(fenetre): 
+    """ Trace tous les triangles définis dans la variable map par le sigle I """
     global position_spike,deffilx
     for i in range (len(position_spike)):
         pygame.draw.polygon(fenetre,noire_mort,[(position_spike[i][0]-deffilx,position_spike[i][1]),(position_spike[i][0]+70-deffilx,position_spike[i][1]),(position_spike[i][0]+35-deffilx,position_spike[i][1]-70)])
         
-def draw_block(fenetre):
-    global position_block,deffilx
+def draw_block():
+    global position_block,deffilx,block
     for i in range (len(position_block)):
         pygame.draw.rect(fenetre,noire,(position_block[i][0]-deffilx,position_block[i][1],70,70))
         pygame.draw.rect(fenetre,noire_mort,(position_block[i][0]-deffilx,position_block[i][1]+1,2,69))
-
+        fenetre.blit(block,(position_block[i][0]-deffilx+1,position_block[i][1]+1))
 
 def verification_collision_saut():
-    global yjoueur,gravité,vitesse_y,chute,noire
+    global yjoueur,vitesse_y,chute,noire
     chute+=1
     for i in range(abs(floor(vitesse_y))):
         if vitesse_y >= 0:
@@ -132,7 +139,6 @@ def jeu():
             fenetre.blit(img_relance,(600,150))
             img_pause = text_police.render("Pause",True,noire)
             fenetre.blit(img_pause,(700,100))
-            print(yjoueur)
 
         else:                     
             vitesse_y+=gravité
@@ -141,7 +147,7 @@ def jeu():
             joueurs.draw(fenetre,yjoueur)
             sol1.draw(fenetre)
             draw_spikes(fenetre)
-            draw_block(fenetre)
+            draw_block()
             deffilx+=1
             img_pause2 = text_police.render("appuie sur 'p' pour faire pause",True,noire)
             fenetre.blit(img_pause2,(100,50))
@@ -175,17 +181,17 @@ class sol(pygame.sprite.Sprite):
     def draw(self,fenetre):
        fenetre.blit(self.image,self.rect)
 
-class block(pygame.sprite.Sprite):
-    def __init__(self,x,y):
-        self.image=pygame.Surface((50,50))
-        self.image2=pygame.Surface((5,49))
-        self.image2.fill(noire_mort)
-        self.image.fill(noire)
-        self.rect=self.image.get_rect(x=x,y=y)
-        self.rect2=self.image2.get_rect(x=x,y=y)
-    def draw(self, fenetre):
-        fenetre.blit(self.image,self.rect)
-        fenetre.blit(self.image2,self.rect2)
+# class block(pygame.sprite.Sprite):
+#     def __init__(self,x,y):
+#         self.image=pygame.Surface((50,50))
+#         self.image2=pygame.Surface((5,49))
+#         self.image2.fill(noire_mort)
+#         self.image.fill(noire)
+#         self.rect=self.image.get_rect(x=x,y=y)
+#         self.rect2=self.image2.get_rect(x=x,y=y)
+#     def draw(self, fenetre):
+#         fenetre.blit(self.image,self.rect)
+#         fenetre.blit(self.image2,self.rect2)
         
 #Boucle principal
 
